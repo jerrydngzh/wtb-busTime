1a2881317973ef9da5df22688db9e26725aef107
jerrydengz <jerrydeng67@gmail.com>
2022-01-15 16:07:07 -0800

    Implement button to get device phone number

--- app/src/main/java/nwHacks2022/bustimeapp/MainActivity.java
+++ app/src/main/java/nwHacks2022/bustimeapp/MainActivity.java
@@ -1,17 +1,34 @@
 package nwHacks2022.bustimeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.view.View;
+import android.telephony.SmsManager;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.credentials.Credential;
+import com.google.android.gms.auth.api.credentials.Credentials;
+import com.google.android.gms.auth.api.credentials.CredentialsApi;
+import com.google.android.gms.auth.api.credentials.CredentialsClient;
+import com.google.android.gms.auth.api.credentials.CredentialsOptions;
+import com.google.android.gms.auth.api.credentials.HintRequest;
 
 import nwHacks2022.bustimeapp.view.AddStopsActivity;
 import nwHacks2022.bustimeapp.view.ListStopsActivity;
 import nwHacks2022.bustimeapp.view.ReadNfcActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String phoneNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,5 +56,66 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), ReadNfcActivity.class);
             startActivity(intent);
         });
+
+        Button sendSMSBtn = findViewById(R.id.send_sms);
+        sendSMSBtn.setOnClickListener(v -> {
+            // if permissions false, ask
+            // grab phone number
+            getDevicePhoneNumber();
+
+            // send text if has phone number
+//            if (phoneNum != null){
+//
+//            }else{
+//                Toast.makeText(this, "No phone numbers found to text the bus!", Toast.LENGTH_LONG).show();
+//            }
+
+        });
+
+    }
+
+    private void getDevicePhoneNumber() {
+        requestHint();
+    }
+
+    // Construct a request for phone numbers and show the picker
+    private void requestHint() {
+        HintRequest hintRequest = new HintRequest.Builder()
+                .setPhoneNumberIdentifierSupported(true)
+                .build();
+
+        CredentialsOptions options = new CredentialsOptions.Builder()
+                .forceEnableSaveDialog()
+                .build();
+
+        CredentialsClient credentialsClient = Credentials.getClient(getApplicationContext(), options);
+        PendingIntent intent = credentialsClient.getHintPickerIntent(hintRequest);
+        try {
+            startIntentSenderForResult(
+                    intent.getIntentSender(), 1
+                    , null, 0, 0, 0, new Bundle()
+            );
+        } catch (IntentSender.SendIntentException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Obtain the phone number from the result
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            // get data from the dialog
+            Credential cred = data.getParcelableExtra(Credential.EXTRA_KEY);
+
+            // set the received data t the text view
+            TextView phoneNum = findViewById(R.id.phone_num_tv);
+            phoneNum.setText(cred.getId().substring(3));
+
+        } else if (requestCode == 1 && resultCode == CredentialsApi.ACTIVITY_RESULT_NO_HINTS_AVAILABLE) {
+            Toast.makeText(this, "No phone numbers found", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
