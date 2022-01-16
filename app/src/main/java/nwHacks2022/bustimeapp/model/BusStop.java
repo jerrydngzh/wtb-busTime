f86a645aeff37f7f14a09b566c708256fc516381
JoshuaJLi <JoshuaJLi@outlook.com>
2022-01-15 16:00:02 -0800

    Add code for bus stop and bus stop manager. Also works in list view.

--- app/src/main/java/nwHacks2022/bustimeapp/model/BusStop.java
+++ app/src/main/java/nwHacks2022/bustimeapp/model/BusStop.java
@@ -1,4 +1,70 @@
 package nwHacks2022.bustimeapp.model;
 
+import java.util.Locale;
+
 public class BusStop {
+    String name;
+    int busNumber;
+    int busStop;
+
+    String latitude;
+    String longitude;
+
+    String nfcCode;
+
+    public BusStop(String name, int busNumber, int busStop, String latitude, String longitude, String nfcCode) {
+        this.name = name;
+        this.busNumber = busNumber;
+        this.busStop = busStop;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.nfcCode = nfcCode;
+    }
+
+    public int getBusNumber() {
+        return busNumber;
+    }
+
+    public void setBusNumber(int busNumber) {
+        this.busNumber = busNumber;
+    }
+
+    public int getBusStop() {
+        return busStop;
+    }
+
+    public void setBusStop(int busStop) {
+        this.busStop = busStop;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTextMessage() {
+        String textFormat = "%d %d";
+        return String.format(Locale.CANADA, textFormat, busNumber, busStop);
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setCoords(String longitude, String latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+
+    public String getId() {
+        return nfcCode;
+    }
 }
--- app/src/main/java/nwHacks2022/bustimeapp/view/ListStopsActivity.java
+++ app/src/main/java/nwHacks2022/bustimeapp/view/ListStopsActivity.java
@@ -1,22 +1,65 @@
 package nwHacks2022.bustimeapp.view;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import nwHacks2022.bustimeapp.R;
+import nwHacks2022.bustimeapp.controller.StopManager;
+import nwHacks2022.bustimeapp.model.BusStop;
 
 public class ListStopsActivity extends AppCompatActivity {
+    private StopManager stopManager = StopManager.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_stops);
+
+        stopManager.add(new BusStop("Hello", 10, 10, "1--", "100", "00"));
+
+        showStops();
+    }
+
+    private void showStops() {
+        ArrayAdapter<BusStop> stopAdapter = new StopListAdapter();
+        ListView busList = findViewById(R.id.stops_listview);
+        busList.setAdapter(stopAdapter);
     }
 
     public static Intent makeIntent(Context context) {
         return new Intent(context, ListStopsActivity.class);
+    }
+
+    private class StopListAdapter extends ArrayAdapter<BusStop> {
+
+        public StopListAdapter() {
+            super(ListStopsActivity.this, R.layout.layout_bus_list, stopManager.getAll());
+        }
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View itemView = convertView;
+            BusStop currentStop = stopManager.get(position);
+            if (itemView == null) {
+                itemView = getLayoutInflater().inflate(R.layout.layout_bus_list, parent, false);
+            }
+
+            TextView nameView = itemView.findViewById(R.id.layout_bus_name);
+            nameView.setText(currentStop.getName());
+
+            return itemView;
+        }
+
     }
 }
